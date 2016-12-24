78d42ec85ce8a9499f6a0acd103b7d7fe171609e
chandan <chandan.kumar@yubl.me>
2016-12-24 22:35:19 +0000

    handled serial number for connected device

--- Adb.rb
+++ Adb.rb
@@ -13,21 +13,23 @@ end
 
 # Return the array of connected android devices
 def devices
-	execute("devices").split("\n")
+	execute("devices").split("\n")[1..-1]
 end
 
 # Install the apk to connected android device
 # Params:
 # +apk_path+:: Path for the apk file
-def install(apk_path)
-	execute("install #{apk_path}")		
+def install(apk_path, serial=nil)
+	command= (serial.nil?) ? "install #{apk_path}" : "-s #{serial} install #{apk_path}"
+	execute(command)		
 end
 
 # Uninstall the application from connected android device
 # Params:
 # +apk_package+:: Package of installed application on connected android device
-def uninstall(apk_package)
-	execute("uninstall #{apk_package}")		
+def uninstall(apk_package,serial=nil)
+	command= (serial.nil?) ? "uninstall #{apk_package}": "-s #{serial} uninstall #{apk_package}"
+	execute(command)		
 end
 
 end
