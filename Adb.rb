7fb3eb55b6be7ddee84f1af2d43fdcec83741fc6
chandan <chandan.kumar@yubl.me>
2016-12-24 22:00:15 +0000

    moved the execute command method to module Adb, removed the module Shell

--- Adb.rb
+++ Adb.rb
@@ -1,25 +1,33 @@
-require_relative 'Shell'
+require 'open3'
 
-class Adb
-	include Shell
-
-	# Return the result for command adb devices
-	def devices
-		execute("adb devices")
+module Adb
+	
+# Runs a subprocess, returns the output and raises the exception if error occurs
+def execute(command)
+	stdout, stderr, status = Open3.capture3("adb "+command)
+	if !stderr.empty?
+		raise "#{command} failed with message \n #{stderr}" 
 	end
+	return stdout
+end
 
-	# Install the apk to connected android device
-	# Params:
-  	# +apk_path+:: Path for the apk file
-	def install(apk_path)
-		execute("adb install #{apk_path}")		
-	end
+# Return the array of connected android devices
+def devices
+	execute("devices").split("\n")
+end
 
-	# Uninstall the application from connected android device
-	# Params:
-  	# +apk_package+:: Package of installed application on connected android device
-	def uninstall(apk_package)
-		execute("adb uninstall #{apk_package}")		
-	end
+# Install the apk to connected android device
+# Params:
+# +apk_path+:: Path for the apk file
+def install(apk_path)
+	execute("install #{apk_path}")		
+end
+
+# Uninstall the application from connected android device
+# Params:
+# +apk_package+:: Package of installed application on connected android device
+def uninstall(apk_package)
+	execute("uninstall #{apk_package}")		
+end
 
 end
